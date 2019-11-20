f2d508a0414a34b7f0d7c6fa8007daf707e264dd
astherath <farceriv@gmail.com>
2019-11-19 21:45:38 -0500

    start of native iOS development

---
+++ Blossom/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Blossom
+//
+//  Created by Felipe Arce on 11/19/19.
+//  Copyright © 2019 Felipe Arce. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let title = "Hello, World!"
+    var body: some View {
+        Text(title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
